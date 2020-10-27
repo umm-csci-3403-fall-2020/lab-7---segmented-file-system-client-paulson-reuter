7f45608b417af52a8f1cf1d9432a41cf02e13acc
Shawn Reuter <reute078@morris.umn.edu>
2020-10-27 11:36:14 -0500

    Create getter methods getStatus and getFileID for PacketStructure

--- src/segmentedfilesystem/PacketStructure.java
+++ src/segmentedfilesystem/PacketStructure.java
@@ -7,12 +7,14 @@ public class PacketStructure {
     private byte ID;
     private byte[] pnumber;
     private byte[] data;
-
+    
+    PacketManager packetManager = new PacketManager();
+    
     public PacketStructure(DatagramPacket packet){
         status = packet.getData()[0];
         ID = packet.getData()[1];
         int length = packet.getLength();
-        if(determineType(packet)) {
+        if(packetManager.determineType(packet)) {
             pnumber[0] = packet.getData()[2];
             pnumber[1] = packet.getData()[3];
             data = Arrays.copyOfRange(packet.getData(), 4, length);
@@ -20,5 +22,15 @@ public class PacketStructure {
         else{
             data = Arrays.copyOfRange(packet.getData(),2,length);
         }
+    }
+
+    public Byte getStatus (Byte packet) {
+        this.status = packet;
+        return status;
+    }
+    
+    public Byte getFileID (Byte packet) {
+        this.ID = packet;
+        return ID;
     }
 }
