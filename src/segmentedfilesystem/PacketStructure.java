c984f143b4f39933f2001705f86072626f669988
Shawn Reuter <reute078@morris.umn.edu>
2020-11-01 19:33:22 -0600

    Create method  getPacketData
    Created the method getPacketData to change a byte Array to a ArrayList so that we will be able to add the whole array list to a hash map

--- src/segmentedfilesystem/PacketStructure.java
+++ src/segmentedfilesystem/PacketStructure.java
@@ -1,5 +1,7 @@
 package segmentedfilesystem;
+
 import java.net.DatagramPacket;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class PacketStructure {
@@ -7,27 +9,46 @@ public class PacketStructure {
     private byte ID;
     private byte[] pnumber;
     private byte[] data;
+    int dataBufferLength;
 
     PacketManager manager = new PacketManager();
-    public PacketStructure(DatagramPacket packet){
+
+    public PacketStructure(DatagramPacket packet) {
         status = packet.getData()[0];
         ID = packet.getData()[1];
         int length = packet.getLength();
-        if(manager.determineType(packet)) {
-            data = Arrays.copyOfRange(packet.getData(),2,length);
-        }
-        else{
+        if (manager.isHeaderPacket(packet)) {
+            data = Arrays.copyOfRange(packet.getData(), 2, length);
+            dataBufferLength = data.length;
+        } else {
             pnumber[0] = packet.getData()[2];
             pnumber[1] = packet.getData()[3];
             data = Arrays.copyOfRange(packet.getData(), 4, length);
+            dataBufferLength = data.length;
         }
     }
 
-    public byte getStatus(){return status;}
+    public byte getStatus() {
+        return status;
+    }
 
-    public byte getID(){return ID;}
+    public byte getID() {
+        return ID;
+    }
 
-    public byte[] getPNumber(){return pnumber;}
+    public byte[] getPNumber() {
+        return pnumber;
+    }
 
-    public byte[] getData() {return data;}
+    public byte[] getData() {
+        return data;
+    }
+
+    public ArrayList<Byte> getPacketData() {
+        ArrayList<Byte> outputBytes = new ArrayList<>();
+        for (int i = 0; i < dataBufferLength; i++) {
+            outputBytes.add(data[i]);
+        }
+        return outputBytes;
+    }
 }
