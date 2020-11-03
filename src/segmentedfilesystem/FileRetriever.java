4a20bb81fec972675651fa6c33040e9551fb2679
halleypaulson <70215918+halleypaulson@users.noreply.github.com>
2020-11-02 19:47:17 -0600

    Updated methods according to PacketManager changes

--- src/segmentedfilesystem/FileRetriever.java
+++ src/segmentedfilesystem/FileRetriever.java
@@ -1,8 +1,6 @@
 package segmentedfilesystem;
 import java.net.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class FileRetriever {
 	private String server;
@@ -17,20 +15,22 @@ public class FileRetriever {
 	public void downloadFiles() throws IOException {
 
 		DatagramSocket socket = new DatagramSocket();
-		byte[] buf = new byte[1024];
+		byte[] buf = new byte[1028];
 		InetAddress address = InetAddress.getByName(server);
 		DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
 		socket.send(packet);
 
-		HashMap<String,PacketStructure> packetChest = new HashMap<>();
 		packet = new DatagramPacket(buf, buf.length);
 
-		//generate array of numbers 0-9, create key, check if its already in the hashmap, if true remake key, check until key isn't in hashmap
-		while(manager.allPackagesReceived(packet)){
+		boolean loop = true;
+		while(loop){
 			socket.receive(packet);
 			PacketStructure copy = new PacketStructure(packet);
-			manager.store(copy,packetChest);
+			manager.store(copy);
 
+			if(manager.allPackagesReceived(copy)){
+				loop = false;
+			}
 		}
 	}
 
